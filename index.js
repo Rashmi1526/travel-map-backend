e74ef7468aae69536ccd5a7ad46dd117cd4a17c2
Rashmi1526 <rashmi.19b101039@abes.ac.in>
2022-07-17 19:32:42 +0530

    added backend

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/users");
+const pinRoute = require("./routes/pins");
+
+dotenv.config();
+
+app.use(express.json());
+
+//allowed everyone to access 
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
+
+mongoose 
+ .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+           })   
+ .then(() => console.log("MongoDB connected!"))
+ .catch(err => console.log(err));
+
+app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
+
+
+
+app.listen(  process.env.PORT || 8000, () => {
+  console.log("Backend server is running!");
+});
